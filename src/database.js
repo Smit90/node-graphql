ee22e4e611e8ce655a8485d3a5ea0a97efc4db46
Smit90 <45687440+Smit90@users.noreply.github.com>
2022-10-17 11:17:39 +0530

    prisma and postgreSQL configure with docker

--- src/database.js
+++ src/database.js
@@ -1,27 +1,7 @@
-const students = [
-  {
-    id: 1,
-    email: "test1@gmail.com",
-    fullName: "Test One",
-    dept: "Software Engineering",
-    enrolled: true,
-  },
-  {
-    id: 2,
-    email: "test2@gmail.com",
-    fullName: "Test Two",
-    dept: "Data Engineering",
-    enrolled: true,
-  },
-  {
-    id: 3,
-    email: "test2@gmail.com",
-    fullName: "Test Three",
-    dept: "System Security",
-    enrolled: false,
-  },
-];
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
 
 module.exports = {
-  students,
+  prisma,
 };
